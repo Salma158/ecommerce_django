1b727160b83e1a7ae76a4dfb62b1a1d9d69c5f4c
salma <salmasherif856@gmail.com>
2024-04-05 02:12:24 -0700

    adding some modifications

--- wishlists/admin.py
+++ wishlists/admin.py
@@ -5,6 +5,6 @@ class WishListAdmin(admin.ModelAdmin):
     list_display = ('user', 'product_list')
 
     def product_list(self, obj):
-        return ", ".join([p.productname for p in obj.products.all()])
+        return ", ".join([p.productname for p in obj.product.all()])
 
 admin.site.register(Wishlist, WishListAdmin)
