abd69d6492dbf5c7696d2eb0753bc35f641e8b57
Omnia-Mohamed88 <https://github.com/Omnia-Mohamed88/>
2024-04-13 20:11:39 +0200

    fixed urls and some modifications

--- products/urls.py
+++ products/urls.py
@@ -1,18 +1,3 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.getRoutes, name="Routes"),
-#     path('products/', views.getProducts, name='getProducts'),
-#     path('products/<int:pk>/', views.getProductDetail, name='getProductDetail'),
-#     path('categories/', views.getCategories, name='getCategories'),
-#     path('categories/<int:pk>/', views.getCategoryDetail, name='getCategoryDetail'),
-#     path('categories/<int:category_pk>/products/', views.getProductsByCategory, name='getProductsByCategory'),
-#     path('products/search/<str:query>/', views.productSearch, name='productSearch'),
-#     path('upload-image/', views.upload_image, name='upload_image'),
-
-# ]
-
 
 from django.urls import path
 from . import views
@@ -22,8 +7,7 @@ urlpatterns = [
     path('', views.getRoutes, name="Routes"),
     
     # Product routes
-    # path('products/', views.getProducts, name='getProducts'),
-    path('products/', views.createProduct, name='getProducts'),
+    path('products/', views.getProducts, name='getProducts'),
     path('products/<int:pk>/', views.getProduct, name='getProduct'),
     path('products/<int:pk>/update/', views.updateProduct, name='updateProduct'),
     path('products/<int:pk>/delete/', views.deleteProduct, name='deleteProduct'),
